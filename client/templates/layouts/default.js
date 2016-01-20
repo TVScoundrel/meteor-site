cb99b421833ca2e56e8088e3112d8382531d8530
Tom Van Schoor <tom@tutuka.com>
2016-01-20 22:37:42 +0200

    Clean up the access control code
    Why:
    
    * It is now more robust.

--- client/templates/layouts/default.js
+++ client/templates/layouts/default.js
@@ -1,16 +1,16 @@
 // helpers to make it easy to check conditions
 const loggedIn = () => {
-  return !Meteor.loggingIn() && Meteor.user() != null;
+  return !Meteor.loggingIn() && !!Meteor.user();
 }
 
-const userNotInRole = (authData) => {
-  if (authData.roles != null && authData.roles.length > 0)
-    return !Roles.userIsInRole(Meteor.userId(), authData.roles);
-  return false;
+const userInRole = (authData) => {
+  if (!!authData.roles && authData.roles.length > 0)
+    return Roles.userIsInRole(Meteor.userId(), authData.roles);
+  return true;
 }
 
-const userLoggedInStateNOk = (authData) => {
-  return authData.requireLoggedIn !== loggedIn();
+const userLoggedInStateOk = (authData) => {
+  return authData.requireLoggedIn === loggedIn();
 }
 
 // this function does the actual work
@@ -19,7 +19,7 @@ const handleRedirect = () => {
   let authData = FlowRouter.current().route.group.options.authentication;
   if (
     authData &&
-    (userLoggedInStateNOk(authData) || userNotInRole(authData))
+    (!userLoggedInStateOk(authData) || !userInRole(authData))
   ) {
     FlowRouter.go(authData.redirectPath);
     return true;
